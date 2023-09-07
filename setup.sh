6b93599d0855bded08b490d175f588a11d0965ed
Daniel Manila <daniel@manilas.net>
2023-09-07 08:37:45 -0400

    Adjust bash prompt

--- setup.sh
+++ setup.sh
@@ -4,4 +4,8 @@ CUR_DIR="$(cd "$(dirname "$0")"; pwd)"
 
 ln -s "$CUR_DIR/.tmux.conf"  ~/.tmux.conf
 ln -s "$CUR_DIR/.vimrc" ~/.vimrc
-echo ". $CUR_DIR/bash_setup" >> ~/.bashrc
+BASH_SETUP_FILE=". $CUR_DIR/bash_setup"
+
+if ! grep -Fxq "$BASH_SETUP_FILE" ~/.bash_profile; then
+    echo "$BASH_SETUP_FILE" >> ~/.bash_profile
+fi
