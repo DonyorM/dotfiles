a480b30f0f5650463244949b2852a10f028f195f
Daniel Manila <danielmanila@WTMBP-Daniel-Manila.local>
2023-11-02 10:17:14 -0400

    Initial zsh configuration

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m' 
+NC='\033[0m'
+
 convert_path() {
     if [ "$OSTYPE" = "msys" ]; then
         echo "${1/\/c\//C:\/}"
@@ -24,3 +29,22 @@ if ! grep -Exq "\s*path ?= ?$GIT_CONFIG_FILE" ~/.gitconfig; then
     git config --global --add include.path "$GIT_CONFIG_FILE"
 fi
 
+if which zsh; then
+    if [ ! -d ~/.oh-my-zsh ]; then
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    fi
+
+    if [ ! -d ~/.oh-my-zsh/custom/themes/powerlevel10k/ ]; then
+        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    fi
+
+    ZSHRC_FILE=". $CUR_DIR/.zshrc"
+    if [[ `readlink ~/.zshrc` != "$ZSHRC_FILE" ]]; then
+        mv ~/.zshrc ~/.zshrc.setup.bak || true
+        ln -s "$ZSHRC_FILE" ~/.zshrc
+    fi
+
+    echo -e "${BLUE}Machine specific zsh configuration goes in ~/.zsh-custom${NC}"
+fi
+
+echo -e "${GREEN}Setup complete!${NC}"
